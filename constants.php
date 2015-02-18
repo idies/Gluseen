e369e28da664b35247af64252f862c1031fbc30f
abcdr2328 <jlian1@students.towson.edu>
2015-02-18 15:30:21 -0500

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,13 @@
+<?php
+  // Single sign-on portal URL
+  define("PORTAL_URL","http://zinc26.pha.jhu.edu:8082");
+  define("KEYSTONE_URL", "http://zinc26.pha.jhu.edu:5000");
+  // Service credentials
+  define("SERVICE_NAME", "gluseen");
+  define("SERVICE_PASSWORD", "gluseen");
+  define("SERVICE_PROJECT", "Services");
+  // Users DB credentials
+  define("DB_CONNECTION","mysql:host='10.55.17.52';dbname='keystone'");
+  define("DB_LOGIN","root");
+  define("DB_PASSWORD","mongoDB42771");
+?>
