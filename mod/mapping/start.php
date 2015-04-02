cf689ca6bfc3e745786dccf1a1b87a7b9db5038f
Yuting <zyt1231@gmail.com>
2015-04-01 20:50:20 -0400

    Update start.php

--- mod/mapping/start.php
+++ mod/mapping/start.php
@@ -1,6 +1,7 @@
 <?php
 	elgg_register_event_handler('init', 'system', 'mapping_init');
- 	
+	$results = file_get_contents('http://yutingsite.com/kml/CSVtoKML.php');
+ 	//getCSV_KML();
 function mapping_init() {
 	elgg_register_page_handler('mapping', 'mapping_page_handler');
 	//add menu item
@@ -26,4 +27,65 @@ $sidebar_alt = elgg_view('mapping/sidebar', array());//left side bar
 	return true;
 	}
 	
+function getCSV_KML() {
+
+$data= file_get_contents('http://dsa002.pha.jhu.edu/EarthScience/EarthScience/getData?Query=select*%20from%20plot&format=csv');
+$rows = explode("\n",$data); //函数把字符串分割为数组。
+//echo var_dump($rows);
+$s = array(); //array() 创建数组，带有键和值。
+foreach($rows as $row) {
+	if($row!=null) {
+    				$s[] = str_getcsv($row);
+    			}
+}
+unset($s[0]); 
+//print_r($s);
+// printf("<br>"); 
+// printf("<br>"); 
+// echo print_r($s[1]);
+$url =  elgg_get_site_url	(	 	$site_guid = 0	);	
+
+//echo "<script type='text/javascript'>alert('$url');</script>";	
+
+$dom = new DOMDocument('1.0', 'UTF-8');  
+$dom->formatOutput = true; 
+$kml = $dom->createElementNS('http://earth.google.com/kml/2.1', 'kml');
+$dom->appendChild($kml);
+//$kml = $dom->createElement("kml");
+$document = $dom->createElement("Document");
+$dname = $dom->createElement("name","KmlFile");  
+$document->appendChild($dname);
+
+foreach ($s as $inner) {  //遍历数组
+
+
+		$placemark = $dom->createElement("Placemark");
+		
+		if($inner[0] == 2) {
+			$name = $dom->createElement("name","601 Woodbourne &amp; Ready Ave"); 
+		}
+		else{
+			$name = $dom->createElement("name",$inner[2]); 
+		}
+		$style = $dom->createElement("styleUrl", "#style"); 
+		$placemark->appendChild($name); 
+
+		$point =$dom->createElement("Point"); 
+		$coordinates="$inner[4]".","."$inner[3]";
+		$coordinates =$dom->createElement("coordinates", $coordinates); 
+		$point->appendChild($coordinates);
+	
+		$placemark->appendChild($style); 
+	    $placemark->appendChild($point);
+		$document->appendChild($placemark);
+
+		$kml->appendChild($document);
+		$dom->appendChild($kml);
+
+		 //cho $dom->saveXML() ;
+		$filename = "baltimoresite.kml"; 
+		$dom->save($filename); 
+
+}
+}
 ?>
