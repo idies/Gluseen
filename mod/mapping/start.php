4c2a8f922a6fea511d55350591522b7f87e27d3a
Yuting <zyt1231@gmail.com>
2015-12-03 12:21:48 -0500

    Update start.php

--- mod/mapping/start.php
+++ mod/mapping/start.php
@@ -1,6 +1,7 @@
 <?php
 	elgg_register_event_handler('init', 'system', 'mapping_init');
-	$results = file_get_contents('http://yutingsite.com/queryGluseen/CSVtoKML.php'); //load KML file
+	$results = file_get_contents('http://scitest09.pha.jhu.edu/elgg/CSVtoKML.php');//load KML file
+	//$results = file_get_contents('http://yutingsite.com/queryGluseen/CSVtoKML.php'); 
  	//getCSV_KML();
 
 function mapping_init() {
